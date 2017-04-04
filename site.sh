1172e60d8d1e9f9fe62fa167e2ece0ff6e08968c
Ronak Shah <ronakshahultimate@gmail.com>
2017-04-03 21:51:01 -0700

    Fixed

--- site.sh
+++ site.sh
@@ -4,5 +4,5 @@ mkdir img
 touch index.html
 touch js/main.js
 touch css/main.css
-echo "<!DOCTYPE html\n\n<html>\n    <head>\n        <link rel='stylesheet' href='css/main.css type='text/css'\n    </head>\n\n    <body>\n\n    </body>\n\n
+echo "<!DOCTYPE html>\n\n<html>\n    <head>\n        <link rel='stylesheet' href='css/main.css' type='text/css' />\n    </head>\n\n    <body>\n\n    </body>\n\n
     <script src='js/main.js'></script>\n\n</html>" >> index.html
